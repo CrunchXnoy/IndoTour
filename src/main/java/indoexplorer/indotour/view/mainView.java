395ff16cc108627bb6f93cf097b7428c5dddff78
Maulidann <maulidannn360@gmail.com>
2022-04-19 22:37:04 +0700

    menambahkan form view

--- src/main/java/indoexplorer/indotour/view/mainView.java
+++ src/main/java/indoexplorer/indotour/view/mainView.java
@@ -36,6 +36,10 @@ public class mainView extends javax.swing.JFrame {
         btnAbout = new javax.swing.JLabel();
         btnLogout = new javax.swing.JLabel();
         mainPanel = new javax.swing.JPanel();
+        homePanel = new javax.swing.JPanel();
+        jakartaPanel = new javax.swing.JPanel();
+        jogjaPanel = new javax.swing.JPanel();
+        baliPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -101,13 +105,65 @@ public class mainView extends javax.swing.JFrame {
                 .addComponent(btnBali)
                 .addGap(39, 39, 39)
                 .addComponent(btnAbout)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 157, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnLogout)
                 .addGap(45, 45, 45))
         );
 
         mainPanel.setBackground(new java.awt.Color(255, 255, 255));
         mainPanel.setLayout(new java.awt.CardLayout());
+
+        javax.swing.GroupLayout homePanelLayout = new javax.swing.GroupLayout(homePanel);
+        homePanel.setLayout(homePanelLayout);
+        homePanelLayout.setHorizontalGroup(
+            homePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        homePanelLayout.setVerticalGroup(
+            homePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        mainPanel.add(homePanel, "card2");
+
+        javax.swing.GroupLayout jakartaPanelLayout = new javax.swing.GroupLayout(jakartaPanel);
+        jakartaPanel.setLayout(jakartaPanelLayout);
+        jakartaPanelLayout.setHorizontalGroup(
+            jakartaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jakartaPanelLayout.setVerticalGroup(
+            jakartaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        mainPanel.add(jakartaPanel, "card3");
+
+        javax.swing.GroupLayout jogjaPanelLayout = new javax.swing.GroupLayout(jogjaPanel);
+        jogjaPanel.setLayout(jogjaPanelLayout);
+        jogjaPanelLayout.setHorizontalGroup(
+            jogjaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jogjaPanelLayout.setVerticalGroup(
+            jogjaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        mainPanel.add(jogjaPanel, "card4");
+
+        javax.swing.GroupLayout baliPanelLayout = new javax.swing.GroupLayout(baliPanel);
+        baliPanel.setLayout(baliPanelLayout);
+        baliPanelLayout.setHorizontalGroup(
+            baliPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        baliPanelLayout.setVerticalGroup(
+            baliPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        mainPanel.add(baliPanel, "card5");
 
         javax.swing.GroupLayout bodyPanelLayout = new javax.swing.GroupLayout(bodyPanel);
         bodyPanel.setLayout(bodyPanelLayout);
@@ -116,8 +172,8 @@ public class mainView extends javax.swing.JFrame {
             .addGroup(bodyPanelLayout.createSequentialGroup()
                 .addComponent(menuPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(694, Short.MAX_VALUE))
+                .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE)
+                .addGap(694, 694, 694))
         );
         bodyPanelLayout.setVerticalGroup(
             bodyPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -175,6 +231,7 @@ public class mainView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel baliPanel;
     private javax.swing.JPanel bodyPanel;
     private javax.swing.JLabel btnAbout;
     private javax.swing.JLabel btnBali;
@@ -182,7 +239,10 @@ public class mainView extends javax.swing.JFrame {
     private javax.swing.JLabel btnJakarta;
     private javax.swing.JLabel btnJogja;
     private javax.swing.JLabel btnLogout;
+    private javax.swing.JPanel homePanel;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jakartaPanel;
+    private javax.swing.JPanel jogjaPanel;
     private javax.swing.JPanel mainPanel;
     private javax.swing.JPanel menuPanel;
     // End of variables declaration//GEN-END:variables
